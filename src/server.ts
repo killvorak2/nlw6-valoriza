eef7988279b8740fe39632fe186155fc9f2f310f
Marcelo <marcelo.ceribelli@gmail.com>
2021-09-01 18:39:35 -0300

    primeiros testes com métodos GET / POST

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,25 @@ import express from "express";
 
 const app = express();
 
+/* 
+Inicio => Teste na construção de métodos
+*/
+
+//Método de GET
+app.get("/test", (request, response) => {
+  return response.send("Olá")
+})
+
+//Método de POST
+app.post("/test-post", (request, response) => {
+  return response.send("Olá usando o método de POST")
+})
+
+/* 
+Final => Teste na construção de métodos
+*/
+
+// Escuta do Servidor na porta definida
 app.listen(3000, () => console.log("Server is running"));
+
+
