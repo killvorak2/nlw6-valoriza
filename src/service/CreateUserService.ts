faf19b23179c2ae937c5e5c8b59887d29123bcc9
Marcelo <marcelo.ceribelli@gmail.com>
2021-09-05 11:25:49 -0300

    adicionada a camada de service do usuário

---
+++ src/service/CreateUserService.ts
@@ -0,0 +1,43 @@
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
+// Criando a interface com os dados a serem usados para usuários
+interface IUserRequest{
+  name: string;
+  email: string;
+  admin?: boolean;
+}
+//Todas as regras para criação de usuários
+class CreateUserService {
+  async execute({name, email, admin}: IUserRequest) {
+    const usersRepository = new UsersRepositories();
+    
+    //verifica se esta sem email
+    if (!email) {
+      throw new Error("Email incorrect");
+    }
+
+    //verifica se esse email já existe no cadastro
+    const userAlreadyExists = await usersRepository.findOne({
+      email
+    })
+
+    //Se o email já estiver cadastrado para alguem, ele devolve o erro com o nome de usuario 
+    if (userAlreadyExists) {
+      throw new Error(`User ${name} already exists`);
+    }
+
+    // criando instancia do usuario para salvar
+    const user = usersRepository.create({
+      name,
+      email,
+      admin
+    })
+
+    //recebe o objeto user para salvar
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export { CreateUserService};
