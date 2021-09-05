dd24a5d27a47d176ce43a1f9d58ac4e0c8b72622
Marcelo <marcelo.ceribelli@gmail.com>
2021-09-05 11:55:59 -0300

    criada a camada de controller e a primeira rota usando o routes.ts, ajustado o server.ts

---
+++ src/controllers/CreateUserController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+import { CreateUserService } from "../service/CreateUserService";
+
+class CreateUserController{
+
+  //recebendo os dados através do Body
+  async handle(request: Request, response: Response) {
+    const { name, email, admin } = request.body;
+
+    const createUserService = new CreateUserService();
+
+    const user = await createUserService.execute({ name, email, admin });
+    
+    //retornando um json com as informações acima, dentro do objeto user
+    return response.json(user);
+  }
+}
+
+export { CreateUserController }
--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,15 @@
 import "reflect-metadata";
 import express from "express";
 
+//importando as rotas do arquivo ROUTES.TS
+import { router } from "./routes";
+
 import "./database";
 
 const app = express();
 
-/* 
-Inicio => Teste na construção dos métodos
-*/
-
-//Método de GET
-app.get("/test", (request, response) => {
-  return response.send("Olá")
-})
-
-//Método de POST
-app.post("/test-post", (request, response) => {
-  return response.send("Olá usando o método de POST")
-})
-
-/* 
-Final => Teste na construção dos métodos
-*/
+// Tornando agora uma middleware
+app.use(router);
 
 // Escuta do Servidor na porta definida
 app.listen(3000, () => console.log("Server is running"));
